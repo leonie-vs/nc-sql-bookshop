a040623aff4ac88d5fc27d08cc0e82c7e2f94dee
Leonie von Schirach <leonie.schirach@web.de>
2025-10-13 15:14:13 +0100

    complete sprint task 6 by reducing price of any excess stock books

--- db/03_insert_books.sql
+++ db/03_insert_books.sql
@@ -7,10 +7,10 @@ VALUES
 (3, 'The Unbearable Lightness of Being', 850, 40, '1988-02-05', True),
 (4, 'The Bell Jar', 799, 70, '1963-01-14', True),
 (5, 'The Handmaid''s Tale', 899, 0, '1985-08-01', True), 
-(6, 'The Tale of Peter Rabbit', 599, 300, '1902-10-01', True),
+(6, 'The Tale of Peter Rabbit', 599, 8, '1902-10-01', True),
 (7, 'Emma', 522, 150, '1815-12-23', True),
 (8, 'Nineteen Eighty-Four: A Novel', 799, 54, '1949-06-08', True),
-(9, 'Pride and Prejudice', 699, 60, '1813-01-28', True),
+(9, 'Pride and Prejudice', 699, 5, '1813-01-28', True),
 (10, 'The Hitchhiker''s Guide to the Galaxy', 899, 180, '1997-10-12', True);
 
 --querying books table after inserting book data
--- db/06_update_books.sql
+++ db/06_update_books.sql
@@ -1,3 +1,6 @@
 \c my_bookshop
 
 \echo '\n These books have been put on sale, due to excess stock:\n'
+
+UPDATE books SET price_in_pence = price_in_pence * 0.9 WHERE quantity_in_stock > 10;
+SELECT * FROM books WHERE quantity_in_stock > 10;
