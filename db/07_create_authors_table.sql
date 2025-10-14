58c6774bf70604ff57bc8f9c4b7742027fcf5905
Leonie von Schirach <leonie.schirach@web.de>
2025-10-14 13:05:49 +0100

    solve sprint task 8: insert author data into authors table

--- db/07_create_authors_table.sql
+++ db/07_create_authors_table.sql
@@ -1,7 +1,7 @@
 \c my_bookshop
 
 CREATE TABLE authors (
-    author_id SERIAL PRIMARY KEY,
+    author_id SERIAL PRIMARY KEY, 
     author_name VARCHAR(40) NOT NULL,
-    fun_fact VARCHAR(100) NOT NULL
+    fun_fact VARCHAR(300) NOT NULL
 );
--- db/08_insert_authors.sql
+++ db/08_insert_authors.sql
@@ -1,1 +1,18 @@
 \c my_bookshop
+
+INSERT INTO authors(author_name, fun_fact)
+VALUES 
+('Stephen Hawking', 'Doctors told him he wouldn''t live past his early 20s.'),
+('Mary Shelley', 'She wrote Frankenstein at age 19.'),
+('Milan Kundera', 'He was born on April Fool''s day in 1929.'),
+('Sylvia Plath', 'Her first poem was published when she was 8 years old.'),
+('Margaret Atwood', 'Atwood was the first author to contribute to The Future Library Project, which will take one writer''s contribution each year for one hundred years to be printed in the year 2114.'), 
+('Beatrix Potter', 'Between 1881 and 1897, Potter kept a journal in which she jotted down her private thoughts in a secret code. This code was so fiendishly difficult, it was not cracked and translated until 1958.'),
+('Jane Austen', 'The author of her first novel, Sense and Sensibility, was simply "A Lady", and her later works like Pride and Prejudice were credited to "the Author of Sense and Sensibility". She wasn''t named as the author of her novels until after her death!'),
+('George Orwell', 'Orwell intentionally got himself arrested for being "drunk and incapable".'),
+('Douglas Adams', 'He made two appearances in Monty Python''s Flying Circus.'), 
+('J. D. Salinger', 'The Catcher in the Rye was the only novel that J.D. Salinger published during his lifetime. Not bad for a first try!'),
+('C. S. Lewis', 'Lewis set up a charitable trust to give away whatever money he received from his books.');
+
+--querying authors table after inserting author data
+SELECT * FROM authors;
