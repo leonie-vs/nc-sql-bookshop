6b45c217902d8e111b402dc19d3ff0bc7f99f528
Leonie von Schirach <leonie.schirach@web.de>
2025-10-14 15:33:28 +0100

    sprint task 13: insert gernes into tabele

--- db/13_insert_genres.sql
+++ db/13_insert_genres.sql
@@ -1,1 +1,14 @@
 \c my_bookshop
+
+INSERT INTO genres(genre)
+VALUES 
+('Science Fiction'),
+('Children''s'),
+('Romance'),
+('Fantasy'),
+('Dystopian'),
+('Science'),
+('Adventure'),
+('Classics');
+
+SELECT * FROM genres;
