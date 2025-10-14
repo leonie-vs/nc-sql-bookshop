900a09e0cdc7efa77d0cbbf5c4a8a9c0d5157b7c
Leonie von Schirach <leonie.schirach@web.de>
2025-10-14 16:43:54 +0100

    sprint task 16: filter tables using JOIN to show books by genre or genres by book

--- db/16_query_books_with_genres.sql
+++ db/16_query_books_with_genres.sql
@@ -2,6 +2,16 @@
 
 \echo '\n Here are the genres for Emma:\n'
 
-
+SELECT books.title, genres.genre 
+FROM books 
+JOIN books_genres ON books.book_id = books_genres.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE books.title = 'Emma';
 
 \echo '\n Here are all the dystopian books:\n'
+
+SELECT genres.genre, books.title
+FROM books 
+JOIN books_genres ON books.book_id = books_genres.book_id
+JOIN genres ON books_genres.genre_id = genres.genre_id
+WHERE genres.genre = 'Dystopian';
