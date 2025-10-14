49296f88a3855d671a9da5e948f7407f370307bf
Leonie von Schirach <leonie.schirach@web.de>
2025-10-14 15:15:26 +0100

    sprint task 11: show book title linked to authors and show authors not associated with a book

--- db/11_select_books_with_author.sql
+++ db/11_select_books_with_author.sql
@@ -2,6 +2,8 @@
 
 \echo '\n Here is a list of book titles with the corresponding author:\n'
 
-
+SELECT title, author_name FROM books JOIN authors ON books.author_id = authors.author_id;
 
 \echo '\n Here is a list of authors without an associated book:\n'
+
+SELECT author_name FROM authors WHERE author_id NOT IN (SELECT DISTINCT author_id FROM books);
